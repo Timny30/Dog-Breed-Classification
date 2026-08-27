0bf8ea6a84566957621a073956c97bd0ce6e88a3
Timny-NCS <chongshengtimothy@gmail.com>
2026-08-27 07:00:06 +0000

    combining three model parser argument with choice

--- get_input_args.py
+++ get_input_args.py
@@ -36,15 +36,14 @@ def get_input_args():
     """
     # Create Parse using ArgumentParser
     parser = argparse.ArgumentParser()
-    
-    # Argument 1: Image Folder 
-    parser.add_argument('--dir', type=str, default = 'pet_images/', help = 'path to the folder of pet images')
-    # Argument 2: CNN Model Architecture
-    parser.add_argument('--arch', type = str, default = 'vgg', help = 'path for accessing CNN model architecture')
-    # Argument 3: Dog Names Text File
-    parser.add_argument('--dogfile', type = str, default = 'dognames.txt', help = 'path for accessing dog images')
-    
-    
+
+    # ensures only 3 valid model can be run for classification
+    parser.add_argument(
+        '--arch', type=str, default='vgg',
+        choices=['vgg', 'alexnet', 'resnet'],
+        help='CNN model architecture'
+        )
+
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
     return parser.parse_args()
